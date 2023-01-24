8cf82bafec36fa80d363104fb33c1ae8a683ab3c
Isaac <isaac.lin.pnc@gmail.com>
2023-01-24 10:18:08 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    let num1 = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      let num2 = array[j]
+      if (num1 + num2 === target) {
+        return true
+      }
+    }
+  }
+  return false;
 }
 
-/* 
+/*
   Write the Big O time complexity of your function here
+  0(n^2)
 */
 
-/* 
+/*
   Add your pseudocode here
+  iterate through the array and call it i
+    iterate through the iterated array and call it j
+
+  if i plus j equals the target number
+   return true
 */
 
 /*
   Add written explanation of your solution here
+  have 2 inputs, 1 being and array, and the other being a target number. If any sum of two numbers in the array adds up to the target number, return true.
 */
 
 // You can run `node index.js` to view these console logs
